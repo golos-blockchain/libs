ce4b3c4f6875516a6e28d1b4e4787ea7a3df6ab4
1aerostorm <aerostorm1@gmail.com>
2025-09-28 22:46:24 +0300

    Update bs58

--- golos-lib-js/src/auth/ecc/src/address.js
+++ golos-lib-js/src/auth/ecc/src/address.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const config = require('../../../config');
 const hash = require('./hash');
-const base58 = require('bs58');
+const base58 = require('bs58').default;
 
 /** Addresses are shortened non-reversable hashes of a public key.  The full PublicKey is preferred.
     @deprecated
